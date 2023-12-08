22247ba1c109dd4340f15b0dd282e8e263a81bb3
itskeshav2004 <keshav22082004@gmail.com>
2023-12-08 14:45:21 +0530

    Modified rds.tf

--- rds.tf
+++ rds.tf
@@ -2,11 +2,11 @@ resource "aws_db_instance" "default" {
   allocated_storage    = 5
   storage_type         = "gp2"
   engine               = "mysql"
-  engine_version       = "5.7"
+  engine_version       = "8.0"
   instance_class       = "db.t2.micro"
   db_name              = "mydb"
-  username             = "foo"
+  username             = "admin"
   password             = "${file("../rds_pass.txt")}"
-  parameter_group_name = "default.mysql5.7"
+  parameter_group_name = "default.mysql8.0"
   skip_final_snapshot = "true"
 }
